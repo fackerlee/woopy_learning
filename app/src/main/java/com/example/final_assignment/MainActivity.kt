5496baaf2bc6da8d3c2e6d10826c3db2b5796d1f
wstan929 <twsheng5753@gmail.com>
2021-06-13 16:07:27 +0800

    add testing comment

--- app/src/main/java/com/example/final_assignment/MainActivity.kt
+++ app/src/main/java/com/example/final_assignment/MainActivity.kt
@@ -14,6 +14,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
+    //this is testing comment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
