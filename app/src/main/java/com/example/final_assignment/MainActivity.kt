7dbe76acbf741cd246d2617f6c5983515b9df7bb
Yong Wei <yongwei@ksoft.com.my>
2021-06-19 00:14:41 +0800

    Fix the Appbar and label problem

--- app/src/main/java/com/example/final_assignment/MainActivity.kt
+++ app/src/main/java/com/example/final_assignment/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.final_assignment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
@@ -64,8 +65,10 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_learn -> true
+            R.id.action_learn -> {
 
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
