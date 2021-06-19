97092d1aa06bcc68a513951fe209dc4856f6d32f
wstan929 <twsheng5753@gmail.com>
2021-06-19 16:01:06 +0800

    next event

---
+++ app/src/main/java/com/example/final_assignment/Animal.kt
@@ -0,0 +1,3 @@
+package com.example.final_assignment
+
+data class Animal(val text_animal:String, val image:Int, val text_diet:String, val text_habitat:String, val text_lifespan:String, val text_speed:String)
--- app/src/main/java/com/example/final_assignment/WildAnimalsActivity.kt
+++ app/src/main/java/com/example/final_assignment/WildAnimalsActivity.kt
@@ -5,27 +5,66 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.FrameLayout
+import android.widget.*
 
 class WildAnimalsActivity : AppCompatActivity() {
 
+    private lateinit var title_animal: TextView
+    private lateinit var image_animal: ImageView
+    private lateinit var content_diet: TextView
+    private lateinit var content_habitat: TextView
+    private lateinit var content_lifespan: TextView
+    private lateinit var content_speed: TextView
+    private lateinit var button_next: Button
     private lateinit var framelayout_btn_icon_speaker: FrameLayout
+
+    val animals = arrayOf(
+        Animal("Snake"
+            , R.drawable.snake_1, "Carnivores", "Land and water", "10-15 Years", "29 km/h"
+        ),
+        Animal("Red Fox"
+            , R.drawable.icon_04, "Omnivores", "Forests, grasslands, mountains, and deserts", "2-5 Years", "50 km/h"
+        )
+    )
+    private var index:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wild_animals)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        setReference()
+        setListeners()
+        loadBook(0)
+
         framelayout_btn_icon_speaker = findViewById(R.id.framelayout_btn_icon_speaker)
 
         framelayout_btn_icon_speaker.setOnClickListener {
-            var mediaPlayer = MediaPlayer.create(this, R.raw.snake)
+            val mediaPlayer = MediaPlayer.create(this, R.raw.snake)
             mediaPlayer.start()
 
-
         }
+    }
 
+    private fun loadBook(index: Int = 0){
+        title_animal.text = animals[index].text_animal
+        image_animal.setImageResource(animals[index].image)
+        content_diet.text = animals[index].text_diet
+        content_habitat.text = animals[index].text_habitat
+        content_lifespan.text = animals[index].text_lifespan
+        content_speed.text = animals[index].text_speed
+    }
 
+    private fun setListeners() {
+
+        button_next.setOnClickListener {
+            index++
+
+            if (index == animals.size)
+                index = 0;
+
+            loadBook(index)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -34,5 +73,15 @@ class WildAnimalsActivity : AppCompatActivity() {
         return true
     }
 
+    private fun setReference(){
+        title_animal = findViewById(R.id.textview_title_diet)
+        image_animal = findViewById(R.id.image_animals_wild)
+        content_diet = findViewById(R.id.textview_diet)
+        content_habitat = findViewById(R.id.textview_habitat)
+        content_lifespan = findViewById(R.id.textview_lifespan)
+        content_speed = findViewById(R.id.textview_speed)
+        button_next = findViewById(R.id.framelayout_btn_icon_next)
+
+    }
 }
 
