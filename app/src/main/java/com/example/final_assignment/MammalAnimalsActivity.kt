ced54a89bfc131daf3d2c5d9904cf4f3d1b8df7e
Yong Wei <yongwei@ksoft.com.my>
2021-06-21 02:11:40 +0800

    Added sound effect and animation for mammal animals

--- app/src/main/java/com/example/final_assignment/MammalAnimalsActivity.kt
+++ app/src/main/java/com/example/final_assignment/MammalAnimalsActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.media.MediaPlayer
 import android.view.Menu
 import android.widget.*
+import androidx.cardview.widget.CardView
 import render.animations.Attention
+import render.animations.Bounce
 import render.animations.Render
 
 class MammalAnimalsActivity : AppCompatActivity() {
@@ -19,15 +21,35 @@ class MammalAnimalsActivity : AppCompatActivity() {
     private lateinit var button_next: FrameLayout
     private lateinit var framelayout_btn_icon_speaker: FrameLayout
     private lateinit var bg_ice: ImageView
+    private lateinit var cardView_image: CardView
+
+    val render_title = Render(this@MammalAnimalsActivity)
+    val render_image = Render(this@MammalAnimalsActivity)
+    val render_diet = Render(this@MammalAnimalsActivity)
+    val render_habitat = Render(this@MammalAnimalsActivity)
+    val render_lifespan = Render(this@MammalAnimalsActivity)
+    val render_speed = Render(this@MammalAnimalsActivity)
+
 
     val animals = arrayOf(
-        Animal("Monkey"
-            , R.drawable.monkey, "Omnivores", "Tropical rainforests of Asia, Africa, and America", "15-20 Years", "55 km/h"
+        Animal(
+            "Monkey",
+            R.drawable.monkey,
+            "Omnivores",
+            "Tropical rainforests of Asia, Africa, and America",
+            "15-20 Years",
+            "55 km/h"
         ),
-        Animal("Lion"
-            , R.drawable.lion, "Carnivores", "Grassland, savanna, and open woodland", "8-15 Years", "80 km/h"
+        Animal(
+            "Lion",
+            R.drawable.lion,
+            "Carnivores",
+            "Grassland, savanna, and open woodland",
+            "8-15 Years",
+            "80 km/h"
         ),
-        Animal("Elephant"
+        Animal(
+            "Elephant"
             , R.drawable.elephant, "Herbivores", "Deserts, swamps, and highlands in tropical and subtropical", "60-70 Years", "40 km/h"
         ),
         Animal("Giraffe"
@@ -43,12 +65,42 @@ class MammalAnimalsActivity : AppCompatActivity() {
         setListeners()
         loadBook(0)
 
-        framelayout_btn_icon_speaker = findViewById(R.id.framelayout_btn_icon_speaker)
+        render_title.setAnimation(Attention.Shake(title_animal))
+        render_image.setAnimation(Bounce.InLeft(cardView_image))
+        render_diet.setAnimation(Bounce.InLeft(content_diet))
+        render_habitat.setAnimation(Bounce.InLeft(content_habitat))
+        render_lifespan.setAnimation(Bounce.InLeft(content_lifespan))
+        render_speed.setAnimation(Bounce.InLeft(content_speed))
+
+        render_title.setDuration(1000)
+        render_image.setDuration(1000)
+        render_diet.setDuration(1000)
+        render_habitat.setDuration(1000)
+        render_lifespan.setDuration(1000)
+        render_speed.setDuration(1000)
+
+        render_title.start()
+        render_image.start()
+        render_diet.start()
+        render_habitat.start()
+        render_lifespan.start()
+        render_speed.start()
 
         framelayout_btn_icon_speaker.setOnClickListener {
-            val mediaPlayer = MediaPlayer.create(this, R.raw.snake)
-            mediaPlayer.start()
-
+            if (index == 0) {
+                val mediaPlayer = MediaPlayer.create(this, R.raw.monkey)
+                mediaPlayer.start()
+            } else if (index == 1) {
+                val mediaPlayer = MediaPlayer.create(this, R.raw.lion)
+                mediaPlayer.start()
+            } else if (index == 2) {
+                val mediaPlayer = MediaPlayer.create(this, R.raw.elephant)
+                mediaPlayer.start()
+            } else if (index == 3) {
+                val mediaPlayer = MediaPlayer.create(this, R.raw.girrafe)
+                mediaPlayer.start()
+            } else {
+            }
         }
     }
 
@@ -65,11 +117,19 @@ class MammalAnimalsActivity : AppCompatActivity() {
 
         button_next.setOnClickListener {
 
-            val render = Render(this@MammalAnimalsActivity)
+            render_title.setAnimation(Attention.Shake(title_animal))
+            render_image.setAnimation(Bounce.InLeft(cardView_image))
+            render_diet.setAnimation(Bounce.InLeft(content_diet))
+            render_habitat.setAnimation(Bounce.InLeft(content_habitat))
+            render_lifespan.setAnimation(Bounce.InLeft(content_lifespan))
+            render_speed.setAnimation(Bounce.InLeft(content_speed))
 
-            render.setAnimation(Attention.Wobble(title_animal))
-            render.start()
-
+            render_title.start()
+            render_image.start()
+            render_diet.start()
+            render_habitat.start()
+            render_lifespan.start()
+            render_speed.start()
             index++
 
             if (index == animals.size)
@@ -86,7 +146,9 @@ class MammalAnimalsActivity : AppCompatActivity() {
         return true
     }
 
-    private fun setReference(){
+    private fun setReference() {
+        cardView_image = findViewById(R.id.cardView_image)
+        framelayout_btn_icon_speaker = findViewById(R.id.framelayout_btn_icon_speaker)
         title_animal = findViewById(R.id.textview_title)
         image_animal = findViewById(R.id.image_animals)
         content_diet = findViewById(R.id.textview_diet)
